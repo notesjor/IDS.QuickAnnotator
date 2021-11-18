6d558d396bc5942f079fea9a75598d9a20a8aab7
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2021-11-18 15:30:09 +0100

    // JOURNAL

--- IDS.QuickAnnotator.Tool4.ConvertToJournal/Program.cs
+++ IDS.QuickAnnotator.Tool4.ConvertToJournal/Program.cs
@@ -15,36 +15,49 @@ namespace IDS.QuickAnnotator.Tool4.ConvertToJournal
     {
       CorpusExplorerEcosystem.InitializeMinimal();
 
-      var corpus = CorpusAdapterWriteDirect.Create(args[0]);
-      var output = args[0] + ".journal";
-
-      var layerNames = corpus.Layers.Select(x=>x.Displayname).Where(x => x.Contains("(") || x == "Wort").OrderBy(x => x).ToArray();
-
-      Console.WriteLine("Layers found:");
-      foreach (var name in layerNames)
-        Console.WriteLine(name);
-      
-      using (var fs = new FileStream(output, FileMode.Create, FileAccess.Write))
-      using (var buffer = new BufferedStream(fs, 4096))
-      using (var writer = new StreamWriter(buffer, Encoding.UTF8))
+      try
       {
-        writer.WriteLine($"SIGLE\tSentence ID\tToken ID\t{string.Join("\t", layerNames)}");
-        foreach (var dsel in corpus.DocumentGuids)
-        {
-          var mdoc = corpus.GetReadableMultilayerDocument(dsel).ToDictionary(x=>x.Key, x=>x.Value.Select(y=>y.ToArray()).ToArray());
-          var sigle = corpus.GetDocumentMetadata(dsel, "Sigle", "");
-          var first = mdoc.First().Value;
+        var corpus = CorpusAdapterWriteDirect.Create(args[0]);
+        var output = args[0] + ".journal";
 
-          for (var s = 0; s < first.Length; s++)
+        var layerNames = corpus.Layers.Select(x => x.Displayname).Where(x => x.Contains("(") || x == "Wort")
+                               .OrderBy(x => x).ToArray();
+
+        Console.WriteLine("Layers found:");
+        foreach (var name in layerNames)
+          Console.WriteLine(name);
+
+        using (var fs = new FileStream(output, FileMode.Create, FileAccess.Write))
+        using (var buffer = new BufferedStream(fs, 4096))
+        using (var writer = new StreamWriter(buffer, Encoding.UTF8))
+        {
+          writer.WriteLine($"SIGLE\tSentence ID\tToken ID\t{string.Join("\t", layerNames)}");
+          foreach (var dsel in corpus.DocumentGuids)
           {
-            for (var w = 0; w < first[s].Length; w++)
+            var mdoc = corpus.GetReadableMultilayerDocument(dsel)
+                             .ToDictionary(x => x.Key, x => x.Value.Select(y => y.ToArray()).ToArray());
+            var sigle = corpus.GetDocumentMetadata(dsel, "Sigle", "");
+            var first = mdoc.First().Value;
+
+            for (var s = 0; s < first.Length; s++)
             {
-              var values = new List<string> { sigle, s.ToString(), w.ToString() };
-              values.AddRange(layerNames.Select(name => mdoc.ContainsKey(name) ? FixEmptyValue(mdoc[name][s][w]) : "?NA?"));
-              writer.WriteLine(string.Join("\t", values));
+              for (var w = 0; w < first[s].Length; w++)
+              {
+                var values = new List<string> { sigle, s.ToString(), w.ToString() };
+                values.AddRange(layerNames.Select(name => mdoc.ContainsKey(name)
+                                                            ? FixEmptyValue(mdoc[name][s][w])
+                                                            : "?NA?"));
+                writer.WriteLine(string.Join("\t", values));
+              }
             }
           }
         }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine(ex.StackTrace);
+        Console.ReadLine();
       }
     }
 
