e2f9d2b823bb892fdff65f19b819588bb09ed047
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2022-01-14 09:51:34 +0100

    // commit

--- IDS.QuickAnnotator.Tool4.CalcDiff/Program.cs
+++ IDS.QuickAnnotator.Tool4.CalcDiff/Program.cs
@@ -8,20 +8,20 @@ namespace IDS.QuickAnnotator.Tool4.CalcDiff
     static void Main(string[] args)
     {
       var model = new AnnotationModelOffline(args[0]);
-      /*
-      var diff = new ExporterDiff();
-      diff.Export(model, args[1]);
 
       var diffS = new ExporterStrictDiff();
       diffS.Export(model, args[1].Replace(".tsv", "_strict.tsv"));
-
-      var html = new ExporterHtml();
-      html.Export(model, args[1].Replace(".tsv", "_html"));
-      */
+      
       var htmlDiff = new ExporterHtmlDiff();
       htmlDiff.Export(model, args[1].Replace(".tsv", "_htmlDiff"));
 
       /*
+      var diff = new ExporterDiff();
+      diff.Export(model, args[1]);      
+
+      var html = new ExporterHtml();
+      html.Export(model, args[1].Replace(".tsv", "_html"));
+
       var xml = new ExporterXml();
       xml.Export(model, args[1].Replace(".tsv", "_xml"));
 
